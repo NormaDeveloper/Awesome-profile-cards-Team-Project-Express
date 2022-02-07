f1991aa792576c78af6dfa888f48eada74ff211b
Norma <normarivasmadrid@gmail.com>
2022-02-07 13:58:24 +0100

    add uuid and template engine

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 // Importamos los dos módulos de NPM necesarios para trabajar
 const express = require('express');
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 // Creamos el servidor
 const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: "10mb" }));
+server.use(express.json({ limit: '10mb' }));
 
 //Configuramos ejs
-server.set('view engine', 'ejs')
+server.set('view engine', 'ejs');
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 4000;
@@ -18,34 +19,51 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-
-
+const savedCards = [];
 // ENDPOINTS
 //Endpoint Create Cards
 server.post('/card', (req, res) => {
-
-  const responseSuccess = {
-    success: true,
-    cardURL: 'https://awesome-profile-cards.herokuapp.com/card/${cardId}',
-  };
-  const responseError = {
-    success: false,
-    error: 'Error description',
-  };
-
-  if (req.body.palette !== '' && req.body.name !== '' && req.body.job !== '' && req.body.email !== '' && req.body.linkedin !== '' && req.body.github !== '' && req.body.photo !== '') {
-    res.json(responseSuccess)
+  if (
+    req.body.palette !== '' &&
+    req.body.name !== '' &&
+    req.body.job !== '' &&
+    req.body.email !== '' &&
+    req.body.linkedin !== '' &&
+    req.body.github !== '' &&
+    req.body.photo !== ''
+  ) {
+    const newCardData = {
+      ...req.body,
+      id: uuidv4(),
+    };
+    savedCards.push(req.body);
+    const responseSuccess = {
+      success: true,
+      cardURL: `http://localhost:${serverPort}/card/${newCardData.id}`,
+    };
+    console.log(savedCards);
+    res.json(responseSuccess);
   } else {
-    res.json(responseError)
-
+    const responseError = {
+      success: false,
+      error: 'Error description',
+    };
+    res.json(responseError);
   }
 });
 
 //Endpoint Show Cards
 server.get('/card/:cardId', (req, res) => {
-  console.log(req.params)
+  const foundCard = savedCards.find((card) => card.id === req.params.id);
+  res.render('card', foundCard);
+  console.log(foundCard);
+  console.log(req.params);
 });
 
 // Servidor estático
-const staticServerPath = "./src/public-react"
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
+
+// Servidor estático
+const staticServerStyles = './src/public-styles';
+server.use(express.static(staticServerStyles));
