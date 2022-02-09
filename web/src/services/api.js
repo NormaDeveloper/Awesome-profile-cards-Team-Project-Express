d8c0d8bb891672027b19fd1388a7684f742e242c
Martarubio93 <you@example.com>
2022-02-09 13:09:39 +0100

    Resolve bog with icons

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 const callToApi = (data) => {
   // Llamamos al API
-  return fetch('https://project-promo-o-module-4-team5.herokuapp.com/card', {
+  return fetch('/card', {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' },
